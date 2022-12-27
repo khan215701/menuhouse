8829af78aee8d4a09c56a496fffcc336fd3622c8
Khan2406 <82305740+khan215701@users.noreply.github.com>
2022-12-27 13:00:55 +0530

    cart functionality and ajax to increase cart

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     path('marketplace', views.marketplace, name='marketplace'),
     path('<slug:vendor_slug>/', views.vendor_details, name='vendor_details'),
+    
+    # carts url
+    path('add_to_cart/<int:food_id>', views.add_to_cart, name='add_to_cart'),
 ]
