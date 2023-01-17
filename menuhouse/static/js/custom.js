49cb530ec153548290924093e8c30a7ee174a16b
Khan2406 <82305740+khan215701@users.noreply.github.com>
2023-01-17 12:37:54 +0530

    implement ajax to send a data to addHours view

--- menuhouse/static/js/custom.js
+++ menuhouse/static/js/custom.js
@@ -203,4 +203,43 @@ $('.item_qty').each(function(){
 
         }
     }
+
+    // add timining hours 
+    $('.add_hour').on('click', function(e){
+        e.preventDefault();
+        var day = document.getElementById('id_day').value
+        var from_hour = document.getElementById('id_from_hour').value
+        var to_hour = document.getElementById('id_to_hour').value
+        var is_closed = document.getElementById('id_is_closed').checked
+        var csrf_token = $('input[name=csrfmiddlewaretoken]').val()
+        var url = document.getElementById('add_hour_url').value
+        console.log(day, from_hour, to_hour, is_closed, csrf_token, url)
+
+        if(is_closed){
+            is_closed = true
+            condition = "day = !''"
+        }else{
+            is_closed = false
+            condition = "day  != '' && from_hour != '' && to_hour != ''"
+        }
+
+        if(eval(condition)){
+            $.ajax({
+                type : 'POST',
+                url : url,
+                data : {
+                    'day': day,
+                    'from_hour': from_hour,
+                    'to_hour': to_hour,
+                    'is_closed': is_closed,
+                    'csrfmiddlewaretoken': csrf_token,
+                },
+                success : function(response){
+                    console.log(response)
+                }
+            })
+        }else{
+            swal('Please fill all field','', 'info')
+        }
+    })
 });
