8c7b8ad718982b46660e2cff7927a4c6fe774bcb
Khan2406 <82305740+khan215701@users.noreply.github.com>
2023-01-18 19:13:28 +0530

    Dynamic Tax Module Create Model

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Cart
+from .models import Cart, Tax
 # Register your models here.
 
 class CartAdmin(admin.ModelAdmin):
     list_display = ('user', 'foodItem', 'quantity', 'updated_at')
     
+
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('tax_type', 'tax_percentage', 'is_active')
+    
 admin.site.register(Cart, CartAdmin)
+admin.site.register(Tax, TaxAdmin)
