e1416a9ceafff85d6eed6859c4d07d87d26ab20d
Khan2406 <82305740+khan215701@users.noreply.github.com>
2023-01-20 10:49:15 +0530

    Load Cover And Profile Pictures

--- account/context_processor.py
+++ account/context_processor.py
@@ -1,4 +1,6 @@
 from vendor.models import Vendor
+from account.models import profile
+from django.shortcuts import get_object_or_404
 from django.conf import settings
 
 def get_vendor(request):
@@ -8,6 +10,9 @@ def get_vendor(request):
         vendor = None
     return dict(vendor=vendor)
 
+def get_profile_cover(request):
+    user_profile = get_object_or_404(profile, user=request.user)
+    return dict(user_profile=user_profile)
 
 def get_google_api(request):
     return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY}
